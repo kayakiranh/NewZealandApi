3af75c58132b46a67e5b211983bbf3d55f95497b
Hüseyin <huseyin.kayakiran@gmail.com>
2024-01-03 16:55:56 +0300

    role seed

--- NewZealandWalk.API/Data/NzwAuthDbContext.cs
+++ NewZealandWalk.API/Data/NzwAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NewZealandWalk.API.Data
@@ -8,6 +9,18 @@ namespace NewZealandWalk.API.Data
         public NzwAuthDbContext(DbContextOptions<NzwAuthDbContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles = new List<IdentityRole> {
+                new IdentityRole{ Id = "37e6c07a-48c4-422a-8943-07f295372e6a", ConcurrencyStamp = "37e6c07a-48c4-422a-8943-07f295372e6a", Name = "Reader", NormalizedName = "Reader" },
+                new IdentityRole{ Id = "4c7c83f1-f984-4b31-ac29-6b23d2cb71ae", ConcurrencyStamp = "4c7c83f1-f984-4b31-ac29-6b23d2cb71ae", Name = "Writer", NormalizedName = "Writer" },
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
