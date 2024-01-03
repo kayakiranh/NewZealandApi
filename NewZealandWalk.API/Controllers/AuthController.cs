c38bb9c67eec37544ce390245604adccf76daebf
Hüseyin <huseyin.kayakiran@gmail.com>
2024-01-03 17:42:07 +0300

    auth register

---
+++ NewZealandWalk.API/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NewZealandWalk.API.Models.DataTransferObject.RegisterDto;
+
+namespace NewZealandWalk.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto model)
+        {
+            IdentityUser identityUser = new IdentityUser
+            {
+                UserName = model.Username,
+                Email = model.Username
+            };
+
+            IdentityResult newIdentityUser = await _userManager.CreateAsync(identityUser, model.Password);
+            newIdentityUser = await _userManager.AddToRolesAsync(identityUser, model.Roles);
+            if (!newIdentityUser.Succeeded) return BadRequest();
+
+            return Ok();
+        }
+    }
+}
